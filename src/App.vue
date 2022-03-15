a19371f7fb5aa90f62001817f108fea7031d54ae
Cdt <452898807@qq.com>
2022-03-15 23:06:05 +0800

    路由设置

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
   <div id="app">
-    <h1>1321</h1>
+    <h1>拉勾教育</h1>
     <router-view/>
   </div>
 </template>
-
-<style lang="scss">
- div {
-   background-color: $success-color;
- }
-</style>
