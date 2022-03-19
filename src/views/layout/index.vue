cd1e65ff8e9288a0d9de7585561d8889d5e3d93d
Cdt <452898807@qq.com>
2022-03-19 23:06:56 +0800

    layout页面编写

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,15 +1,47 @@
 <template>
-  <div>
-    <router-view></router-view>
-  </div>
+  <el-container>
+    <el-aside width="200px">
+      <app-aside></app-aside>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <app-header></app-header>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
+
+import AppAside from '@/views/layout/components/AppAside'
+import AppHeader from '@/views/layout/components/AppHeader'
+
 export default {
-  name: 'LayoutIndex'
+  name: 'LayoutIndex',
+  components: {
+    AppAside,
+    AppHeader
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.el-container {
+  height: 100vh;
+}
 
+.el-aside {
+  background-color: #d3dce6;
+}
+
+.el-header {
+  background-color: #F8F9FB;
+}
+
+.el-main {
+  background-color: #E9EEF3;
+}
 </style>
